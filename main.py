bee3dca142829561632c1713e3e8b32b020989e5
alipascal <alisaumon.prv@gmail.com>
2025-12-30 23:10:03 +0100

    implementing cleanning

--- main.py
+++ main.py
@@ -12,13 +12,23 @@ def createRDFNode(subj, verb, obj):
 
 
 import re
+import unicodedata
 def cleanEntity(subj, verb, obj):
-
+    
     def clean(text):
-        text = re.sub(r'[^\x20-\x7E]', '', text)
+        text = re.sub(r'[^A-Za-z0-9 ]+', '', text)
         return text.replace(" ", "_")
     
-    return clean(subj), clean(verb), clean(obj)
+    def remove_accents(text):
+        return ''.join(
+            c for c in unicodedata.normalize('NFD', text)
+            if unicodedata.category(c) != 'Mn'
+        )
+    
+    subj, verb, obj = tuple(map(clean, (subj, verb, obj)))
+    subj, verb, obj = tuple(map(remove_accents, (subj, verb, obj)))
+
+    return subj, verb, obj
 
 
 def extractTriplets(text):
@@ -30,7 +40,7 @@ def extractTriplets(text):
         ("médecin", "prescrire", "traitement"),
         ("patient", "prendre", "médicament"),
         ("Le Crime de l'Orient Express", "est", "roman"),
-        ("Le Crime de l'Orient Express", "a été écrit par", "Agatha Christie")
+        ("Crime de l'Orient Express", 'est écrit par', 'Agatha Christie')
     ]
     return entities
 
@@ -62,10 +72,14 @@ if __name__ == '__main__':
     # Text to RDF graph
     graph = Graph()
     text = "None"
-    entities = extractTriplets(text)
-    for subj, verb, obj in entities:
-        subj, verb, obj = cleanEntity(subj, verb, obj)
-        node = createRDFNode(subj, verb, obj)
-        graph.add(node)
+    # Traiter le text phrase par phrase
+    for sentence in text.split("."):
+        if sentence == "":
+            continue
+        entities = extractTriplets(sentence)
+        for subj, verb, obj in entities:
+            subj, verb, obj = cleanEntity(subj, verb, obj)
+            node = createRDFNode(subj, verb, obj)
+            graph.add(node)
     graph.serialize(destination="output.rdf", format="turtle")
     # pour visualiser le graphe : https://www.ldf.fi/service/rdf-grapher 
